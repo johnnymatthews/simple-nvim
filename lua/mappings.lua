47f2d921256b641d8f4b70d9420e2d5b4abaf660
johnnymatthews <9611008+johnnymatthews@users.noreply.github.com>
2024-08-20 07:00:16 -0300

    Closes ToggleTerm instance once I close lazygit.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -34,7 +34,7 @@ end)
 
 -- float terminal
 map("n", "<leader>ft", "<cmd> ToggleTerm direction=float<CR>")
-map("n", "<leader>fg", "<cmd> TermExec cmd='lazygit' direction=float lazygit<CR>")
+map("n", "<leader>fg", "<cmd>TermExec cmd='lazygit && exit' direction=float<CR>")
 
 -- Make :W work like :w and :Q work like :q
 vim.cmd('cnoreabbrev W w')
